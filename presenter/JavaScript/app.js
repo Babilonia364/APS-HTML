706317a2ada9a81dfc109f648a819d76ed1db12b
Joao Victor <joao.victor@dellead.com>
2019-10-12 15:09:25 -0300

    form e path relativo

--- presenter/JavaScript/app.js
+++ presenter/JavaScript/app.js
@@ -1,4 +1,4 @@
-const express =require('express'); //para fzr o backend
+const express = require('express'); //para fzr o backend
 const app = express();
 const mysql = require('mysql');
 const session = require('express-session');
@@ -7,49 +7,62 @@ const path = require('path');
 
 //mudar o usuario e a senha de acordo com o seu mysql
 const connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'apsjob'
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'apsjob'
 });
 
-connection.connect(function(err){
-	if(err){
-	  console.log('Error connecting to Db');
-	  return;
+connection.connect(function (err) {
+	if (err) {
+		console.log('Error connecting to Db');
+		return;
 	}
 	console.log('Connection established');
-  });
-  
+});
 
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//para usar o css,imagens,etc colocar o diretorio onde as pastas estao
- app.use(express.static("C:\\Users\\João Victor\\Desktop\\APS-HTML-developer"));
 
-//ler html, colocar o diretorio e indicar o arquivo html
-app.get("/login",function(req,res){
-    res.sendFile("C:\\Users\\João Victor\\Desktop\\APS-HTML-developer\\index.html");
+//para usar o css,imagens,etc colocar o diretorio onde as pastas estao
+app.use(express.static(path.resolve("../../")));
+
+//  GETS
+app.get("/login", function (req, res) {
+	res.sendFile("index.html");
 });
 
-app.post('/auth', function(request, response){
+app.get("/cadastroaluno", function (req, res) {
+	res.sendFile(path.resolve("../../view/cadastroAluno.html"));
+});
+
+app.get("/cadastrarprof", function (req, res) {
+	res.sendFile(path.resolve("../../view/cadastroProf.html"));
+});
+
+app.get("/home", function (req, res) {
+	res.sendFile(path.resolve("../../view/home.html"));
+});
+
+// POSTS
+app.post('/auth', function (request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		connection.query('SELECT * FROM login WHERE usuario = ? AND senha = ?', [username, password], function(error, results, fields) {
+		connection.query('SELECT * FROM login WHERE usuario = ? AND senha = ?', [username, password], function (error, results, fields) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
 				response.redirect('/home');
 			} else {
 				response.send('Incorrect Username and/or Password!');
-			}			
+			}
 			response.end();
 		});
 	} else {
@@ -58,56 +71,42 @@ app.post('/auth', function(request, response){
 	}
 });
 
-
-app.post('/cadasA', function(req,res) {
-  var nome = req.body.nome;
+app.post('/cadasA', function (req, res) {
+	var nome = req.body.nome;
 	var email = req.body.email;
 	var senha = req.body.senha;
 	var instituicao = req.body.instituicao;
 	var matricula = req.body.matricula;
 
-	connection.query("INSERT INTO `aluno` (nome, email,instituicao,matricula) VALUES (?,?,?,?)", [nome.toString(), email.toString(),instituicao.toString(),matricula.toString()], function(err, result) {
-		if(err) throw err;
+	connection.query("INSERT INTO `aluno` (nome, email,instituicao,matricula) VALUES (?,?,?,?)", [nome.toString(), email.toString(), instituicao.toString(), matricula.toString()], function (err, result) {
+		if (err) throw err;
 	});
 
-	connection.query("INSERT INTO `login` (usuario, senha) VALUES (?,?)", [email.toString(), senha.toString()], function(err, result) {
-    if(err) throw err;
+	connection.query("INSERT INTO `login` (usuario, senha) VALUES (?,?)", [email.toString(), senha.toString()], function (err, result) {
+		if (err) throw err;
 	});
-	
-  res.redirect('/login');
-});
-
-app.post('/cadasP', function(req,res) {
-	var nome = req.body.nome;
-var email = req.body.email;
-var senha = req.body.senha;
-var titulacao = req.body.titulacao;
-var instituicao = req.body.instituicao;
-var areadepesquisa = req.body.areadepesquisa;
-
-	connection.query("INSERT INTO `professor` (nome, email,instituicao,titulacao,area_pesq) VALUES (?,?,?,?,?)", [nome, email,instituicao,titulacao,areadepesquisa], function(err, result) {
-			if(err) throw err;
-});
-
-connection.query("INSERT INTO `login` (usuario, senha) VALUES (?,?)", [email, senha], function(err, result) {
-			if(err) throw err;
-});
 
 	res.redirect('/login');
 });
 
+app.post('/cadasP', function (req, res) {
+	var nome = req.body.nome;
+	var email = req.body.email;
+	var senha = req.body.senha;
+	var titulacao = req.body.titulacao;
+	var instituicao = req.body.instituicao;
+	var areadepesquisa = req.body.areadepesquisa;
 
-app.get("/cadastroaluno",function(req,res){
-    res.sendFile("C:\\Users\\João Victor\\Desktop\\APS-HTML-developer\\view\\cadastroAluno.html" );
+	connection.query("INSERT INTO `professor` (nome, email,instituicao,titulacao,area_pesq) VALUES (?,?,?,?,?)", [nome, email, instituicao, titulacao, areadepesquisa], function (err, result) {
+		if (err) throw err;
+	});
+
+	connection.query("INSERT INTO `login` (usuario, senha) VALUES (?,?)", [email, senha], function (err, result) {
+		if (err) throw err;
+	});
+
+	res.redirect('/login');
 });
 
-app.get("/cadastrarprof",function(req,res){
-    res.sendFile("C:\\Users\\João Victor\\Desktop\\APS-HTML-developer\\view\\cadastroProf.html" );
-});
-
-app.get("/home",function(req,res){
-    res.sendFile("C:\\Users\\João Victor\\Desktop\\APS-HTML-developer\\view\\home.html" );
-});
-
-
-app.listen(8081, function(){console.log("Servidor ligado");});
+// PORTA
+app.listen(8081, function () { console.log("Servidor ligado"); });
